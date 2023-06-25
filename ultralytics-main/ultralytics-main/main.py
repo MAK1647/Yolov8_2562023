1992c3818903ea0a1af66a1e4214d10febe0f9bd
PHẠM HIỂN LONG <56377320+MAK1647@users.noreply.github.com>
2023-06-25 16:23:07 +0700

    Update main.py

--- ultralytics-main/ultralytics-main/main.py
+++ ultralytics-main/ultralytics-main/main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8s.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=1)  # train the model
+results = model.train(data="data1.yaml", imgsz=640 , batch = 16 ,epochs=3)  # train the model
